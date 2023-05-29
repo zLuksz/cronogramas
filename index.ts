ae1d22bc138c175935f3548d5b924988063a15c3
Danielle Almeida <daniellealmeidagms@gmail.com>
2023-05-29 12:03:56 -0300

    servidor online

---
+++ index.ts
@@ -0,0 +1,15 @@
+import { AppDataSource } from "./src/databases/data-source"
+
+const express = require("express")
+const app = express()
+app.use(express.json())
+
+app.get("/", (request, response) => {
+  return response.json("E aí, suave?")
+})
+
+app.listen(3333, () =>
+  console.log("O server tá ON na porta 3333.")
+)
+
+AppDataSource.initialize()

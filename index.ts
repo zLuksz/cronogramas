dee84d19c35b6c3d8518dd78d9031044b467e21e
Marcos Antônio de Almeida <marcosantoniomgy@gmail.com>
2023-05-31 09:17:30 -0300

    checkpoint 31-05

--- index.ts
+++ index.ts
@@ -1,17 +1,13 @@
-import { AppDataSource } from "./src/databases/data-source"
+import { AppDataSource } from "./src/databases/connections/data-source"
+AppDataSource.initialize()
 
 const express = require("express") 
-
 const app = express()
-
 app.use(express.json())
 
 app.get('/', (request, response) => {
-    return response.json("E ai, Beleza?")
+    return response.json("E ai, suave?")
 })
 
-app.listen(3333, () =>
-    console.log("O server esta ON na porta 3333.")
-)
+app.listen(3333, () => console.log("O server esta ON na porta 3333."))
 
-AppDataSource.initialize()

56a8499299d9853121e9989ae525542ac1ae8822
Thyago Arraes <thyagosouzaarraes@gmail.com>
2023-05-31 09:17:39 -0300

    checkpoint 31-05

--- index.ts
+++ index.ts
@@ -1,17 +1,12 @@
-import { AppDataSource } from "./src/databases/data-source"
+import { AppDataSource } from "./src/databases/connections/data-source"
+AppDataSource.initialize()
 
 const express = require("express") 
-
 const app = express()
-
 app.use(express.json())
 
 app.get('/', (request, response) => {
     return response.json("E ai, Beleza?")
 })
 
-app.listen(3333, () =>
-    console.log("O server esta ON na porta 3333.")
-)
-
-AppDataSource.initialize()
+app.listen(3333, () =>  console.log("O server esta ON na porta 3333."))
